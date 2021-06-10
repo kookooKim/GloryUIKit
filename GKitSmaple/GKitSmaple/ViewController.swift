7c4dd07e9b425245e9ac7ddb9ee2bd8b311f6278
gloryKim <ikmujn5@naver.com>
2021-06-10 17:52:20 +0900

    ..

---
+++ GKitSmaple/GKitSmaple/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GKitSmaple
+//
+//  Created by 김영광 on 2021/06/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
